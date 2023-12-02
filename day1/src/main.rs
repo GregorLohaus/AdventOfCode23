e54f99218bdd9c3c785f411b70ed9c842922e611
Gregor Lohaus <gregor.lohaus@8mylez.com>
2023-12-02 16:30:49 +0100

    failed approach no idea whats wrong

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,24 @@
 use regex::Regex;
+use std::collections::HashMap;
 use std::io::{self, BufRead, Write};
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, OnceLock};
 use std::thread;
+fn str_num_to_num_str() -> &'static HashMap<&'static str, &'static str> {
+    static HASHMAP: OnceLock<HashMap<&str, &str>> = OnceLock::new();
+    HASHMAP.get_or_init(|| {
+        let mut h: HashMap<&str, &str> = HashMap::new();
+        h.insert("one", "1");
+        h.insert("two", "2");
+        h.insert("three", "3");
+        h.insert("four", "4");
+        h.insert("five", "5");
+        h.insert("six", "6");
+        h.insert("seven", "7");
+        h.insert("eight", "8");
+        h.insert("nine", "9");
+        h
+    })
+}
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let handle = stdin.lock();
@@ -12,17 +29,55 @@ fn main() -> io::Result<()> {
         let cloned = sum.clone();
         let handle = thread::spawn(move || {
             let mut locked = cloned.lock().unwrap();
-            let re = Regex::new(r"\d").unwrap();
+            let re = Regex::new(r"(\d|one|two|three|four|five|six|seven|eight|nine)").unwrap();
             let mut digits = re.find_iter(l.as_str());
             let mut num = String::new();
             match digits.next() {
-                Some(d1) => match digits.last() {
-                    Some(d2) => num = num + d1.as_str() + d2.as_str(),
-                    None => num = num + d1.as_str() + d1.as_str(),
-                },
-                None => (),
+                Some(d1) => {
+                    let d1_str: &str;
+                    if d1.len() > 1 {
+                        match str_num_to_num_str().get(d1.as_str()) {
+                            Some(str) => d1_str = str,
+                            None => {
+                                dbg!("str_num_to_num_str did not return anythinh");
+                                panic!()
+                            }
+                        }
+                    } else {
+                        d1_str = d1.as_str();
+                    }
+                    match digits.last() {
+                        Some(d2) => {
+                            let d2_str: &str;
+                            if d2.len() > 1 {
+                                match str_num_to_num_str().get(d2.as_str()) {
+                                    Some(str) => d2_str = str,
+                                    None => {
+                                        dbg!("str_num_to_num_str did not return anythinh");
+                                        panic!()
+                                    }
+                                }
+                            } else {
+                                d2_str = d2.as_str();
+                            }
+                            num = num + d1_str + d2_str;
+                            dbg!(l.clone(), num.clone(), d1_str, d2_str);
+                        }
+                        None => {
+                            dbg!(l.clone(), num.clone(), d1_str, d1_str);
+                            num = num + d1_str + d1_str;
+                        }
+                    }
+                }
+                None => panic!(),
             }
-            *locked += num.parse::<i32>().unwrap();
+            if let Ok(int) = num.parse::<i32>() {
+                *locked += int;
+                dbg!(*locked);
+            } else {
+                dbg!("could not parse", num);
+                panic!()
+            }
         });
         handles.push(handle)
     }
