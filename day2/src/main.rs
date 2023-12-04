dc3ddf7aa6f391e8d65d35802e3b7a564be3326a
Gregor Lohaus <gregor.lohaus@8mylez.com>
2023-12-05 00:04:40 +0100

    success

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,31 +1,42 @@
-use std::{
-    collections::HashMap,
-    io::{self, BufRead},
-};
+use std::io::{self, BufRead};
 
 fn main() -> io::Result<()> {
-    let mut rules: HashMap<&str, i32> = HashMap::new();
-    rules.insert("red", 12);
-    rules.insert("blue", 14);
-    rules.insert("green", 13);
     let stdin = io::stdin();
     let handle = stdin.lock();
     let mut sum = 0;
     for line in handle.lines() {
         let line_str = line.ok().unwrap().to_string();
         let mut line_iter = line_str.split(':').into_iter();
-        let game_str = line_iter.next().unwrap().to_string();
-        let id_str = game_str.split(' ').last().unwrap().to_string();
+        let _game_str = line_iter.next().unwrap().to_string();
         let data_sets_str = line_iter.next().unwrap().to_string();
         let data_iter = data_sets_str.split(&[',', ';']).map(|p| p.trim());
-        let invalid = data_iter.filter(|d| {
+        let mut prev_min_red = 0;
+        let mut prev_min_green = 0;
+        let mut prev_min_blue = 0;
+        for d in data_iter {
             let p = d.trim().split(' ').collect::<Vec<&str>>();
-            &(p[0].parse::<i32>().unwrap()) > rules.get(p[1]).unwrap()
-        });
-        let valid = invalid.collect::<Vec<&str>>().len() == 0;
-        if valid {
-            sum += id_str.parse::<i32>().unwrap();
+            let num = p[0].parse::<i32>().unwrap();
+            match p[1] {
+                "red" => {
+                    if num > prev_min_red {
+                        prev_min_red = num;
+                    }
+                }
+                "blue" => {
+                    if num > prev_min_blue {
+                        prev_min_blue = num;
+                    }
+                }
+                "green" => {
+                    if num > prev_min_green {
+                        prev_min_green = num;
+                    }
+                }
+                _ => (),
+            }
         }
+        let pow = prev_min_blue * prev_min_green * prev_min_red;
+        sum += pow;
     }
     dbg!(sum);
     Ok(())
